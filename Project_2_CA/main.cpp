ced873c69338329b4bf3f0210701000ada9037ad
mohamedleithy <leithy@aucegypt.edu>
2021-12-02 14:40:25 +0200

    Initial Commit

---
+++ Project_2_CA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project_2_CA
+//
+//  Created by Mohamed Ellethy on 02/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
